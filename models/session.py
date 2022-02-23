942e172dac7740892f3c5d5549094ded38ee2239
Emerson Rodriguez <josuerodriguezmaradiaga1999@gmail.com>
2022-02-22 21:26:25 -0600

    Aplicacion de herencia y modificacion de vistas desde el desarrollador

--- models/session.py
+++ models/session.py
@@ -10,6 +10,6 @@ class Course(models.Model):
     date = fields.Date(date='Date')
     duration = fields.Float(float='Duration')
     seats = fields.Integer(int='Seats')
-    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    instructor_id = fields.Many2one('res.partner', string="Instructor", domain=['|', ('instructor', '=', True),('category_id.name', 'ilike', "Teacher")])
     course_id = fields.Many2one('open_academy.course',ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
